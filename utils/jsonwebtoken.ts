461a8210563c82f41e14b37aa12fffa897ba6666
th-release <th.release.cth@gmail.com>
2022-11-29 17:03:28 +0900

    feat: `add` env file

--- utils/jsonwebtoken.ts
+++ utils/jsonwebtoken.ts
@@ -1,9 +1,18 @@
 import jsonwebtoken from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
-export function sign() {
+dotenv.config();
 
+export function sign(
+  data:object, 
+  expiresIn: string
+) {
+  return jsonwebtoken.sign(data, process.env.SECRET!.toString(), { expiresIn })
 }
 
-export function verify() {
-  
+export function verify(
+  token: string
+) {
+  return jsonwebtoken.verify(token, process.env.SECRET!.toString())
+
 }
